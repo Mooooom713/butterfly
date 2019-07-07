f91f165913f0b5dbe6729a4494206b38c7ca283e
atmoyxic <1332720619amy@gmail.com>
2019-07-07 17:22:12 +0800

    components

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Package from '@/pages/Package'
-import Right from '@/pages/Right'
+import Package from '@/pages/Package/Package'
+import Right from '@/pages/Right/Right'
 
 Vue.use(Router)
 
