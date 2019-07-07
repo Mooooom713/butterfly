66f69416dfef36b7c1033330baaaa74c18018fc7
mooooom713 <13699497036@163.com>
2019-07-07 23:21:43 +0800

    feat(add): right page

--- src/pages/Right/Right.vue
+++ src/pages/Right/Right.vue
@@ -1,15 +1,37 @@
 <template>
-  <div></div>
+  <div class="right-wrap">
+    <van-nav-bar title="权益详情" left-arrow />
+    <right-title></right-title>
+    <div class="right-menu-block">
+      <p>代言人套餐</p>
+      <p>购超值商品套餐(任选其一)尊享代言人权益</p>
+    </div>
+    <div class="right-detail-block">
+      <p>
+        8大权益
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
+import RightTitle from './components/RightTitle'
+
 export default {
   name: "Right",
+  components: {
+    RightTitle
+  },
   data() {
     return {};
   }
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import "../../scss/navBar.scss";
+
+  .right-wrap{
+    width: 100%;
+  }
 </style>
---
+++ src/pages/Right/components/RightTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="right-title-block">
+    <h3>339元购超值商品获得代言权益</h3>
+    <p>专享8大权益</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'RightTitle'
+}
+</script>
+
+<style lang="scss" scoped>
+  .right-title-block{
+    width: 100%;
+    background: #f3f3f3;
+    height: 83pt;
+  }
+</style>
+
