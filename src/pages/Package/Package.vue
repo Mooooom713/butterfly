0291eb21352c48f7da1be3c0b1cc483046c6917c
atmoyxic <1332720619amy@gmail.com>
2019-07-07 18:15:50 +0800

    navbar

--- src/pages/Package/Package.vue
+++ src/pages/Package/Package.vue
@@ -1,15 +1,50 @@
 <template>
-  <div></div>
+  <div id="package">
+    <van-nav-bar title="套餐专区" left-text="返回" left-arrow />
+    <img :src="imgSrc" />
+    <div id="hint-text">
+      成为代言人
+      <br />尊享精选套餐 8大权益
+    </div>
+    <div id="router-text">了解权益详情 &lt;</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Package",
   data() {
-    return {};
+    return {
+      imgSrc: require("../../assets/header-background.png")
+    };
   }
 };
 </script>
 
-<style scoped>
+<style rel="stylesheet/scss" lang="scss" scoped>
+#package {
+  width: 100%;
+}
+
+#hint-text {
+  position: fixed;
+  top: 50pt;
+  left: 13pt;
+  color: #fde8c9;
+  text-align: left;
+  font-size: 16pt;
+  width: 185pt;
+  height: 44pt;
+}
+
+#router-text {
+  position: fixed;
+  top: 100pt;
+  left: 13pt;
+  color: #fae3c3;
+  text-align: left;
+  font-size: 12pt;
+  width: 87pt;
+  height: 12pt;
+}
 </style>
